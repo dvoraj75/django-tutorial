c4b5ae58f19d61116804680baa39a317ca60d6f9
Jan Dvorak <dvoraj75@gmail.com>
2019-11-19 15:01:09 +0100

    Improving our view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -14,9 +15,12 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return last five published questions
+        Return last five published questions (not including those set to be
+        published in the future)
         """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
